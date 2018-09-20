cccd409ea4b728ae6bf84d1b9a03a497ce1178be
ManuelPluas <42387653+ManuelPluas@users.noreply.github.com>
2018-09-20 13:39:29 -0500

    Login
    Login

---
+++ Land/Land/Land/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Land.Infrastructure
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+        #region properties
+        public MainViewModel Main
+            { 
+            get; 
+            set; 
+            }
+        #endregion
+        #region contructores
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
